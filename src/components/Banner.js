55542bb6da96b260cf4188e552f4ef791eccf92c
VavaIkelman <mr.ilkevich@gmai.com>
2021-11-21 15:50:09 +0300

    Init

---
+++ src/components/Banner.js
@@ -0,0 +1,14 @@
+/**
+ * Компонент "Рекламный горизонтальный баннер"
+ * Принимает props:
+ * - изображение баннера
+ * - ссылка на сайт рекламодателя
+ */
+ export default function Banner({ image, link }) {
+    return (
+      <div>
+        <img src="" alt="image shop"></img>
+        <a href="#0">Link on shop</a>
+      </div>
+    );
+  }
